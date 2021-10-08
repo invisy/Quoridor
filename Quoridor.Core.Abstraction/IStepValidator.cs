4e4726c17db39ac092c8c331a7e223ac6c206506
DenKu1 <ledar54@gmail.com>
2021-10-08 23:15:44 +0300

    Add interface for validating player steps

---
+++ Quoridor.Core.Abstraction/IStepValidator.cs
@@ -0,0 +1,14 @@
+﻿using Quoridor.Core.Abstraction.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor.Core.Abstraction
+{
+    public interface IStepValidator
+    {
+        List<Point> GetPossibleSteps(IBoard board, Point startPoint);
+    }
+}
