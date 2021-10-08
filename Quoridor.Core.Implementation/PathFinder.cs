4db7a0c04b4840dc67e01b4bdfa7fceb312e743c
DenKu1 <ledar54@gmail.com>
2021-10-08 23:15:59 +0300

    Add PathFinder

---
+++ Quoridor.Core.Implementation/PathFinder.cs
@@ -0,0 +1,50 @@
+﻿using Quoridor.Core.Abstraction;
+using Quoridor.Core.Abstraction.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor.Core.Implementation
+{
+    public class PathFinder : IPathFinder
+    {
+        IBoard _board;
+        IStepValidator _stepValidator;
+
+        List<Point> visited = new List<Point>();
+        List<Point> unvisited = new List<Point>();
+
+        public PathFinder(IBoard board, IStepValidator stepValidator)
+        {
+            _board  = board;
+            _stepValidator = stepValidator;
+        }
+
+        //Not optimized to A* yet
+        public bool PathExistsToAny(Point point, List<Point> points)
+        {
+            unvisited.Add(point);
+            while (unvisited.Count > 0)
+            {
+                if (points.Find(x => x.Equals(unvisited[0])) != null)
+                    return true;
+
+                visited.Add(unvisited[0]);
+
+                List<Point> newPoints = _stepValidator.GetPossibleSteps(_board, point);
+
+                foreach (Point newPoint in newPoints)
+                {
+                    if (visited.Find(x => x.Equals(newPoint)) == null &&
+                        unvisited.Find(x => x.Equals(newPoint)) == null)
+                    {
+                        unvisited.Add(newPoint);
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
