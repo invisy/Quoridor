a0c71904fca780317574aaa6c497b6bbf190a470
DenKu1 <ledar54@gmail.com>
2021-10-17 23:00:38 +0300

    Add input validation tools

---
+++ Quoridor.MVC/Utilites/InputValidators.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Quoridor.Core.Abstraction.Common;
+using Quoridor.MVC.Extensions;
+
+namespace Quoridor.MVC.Utilites
+{
+    static class InputValidators
+    {
+        public static bool IsPlaceFenceArgumentsValid(this IList<string> arguments) =>
+            int.TryParse(arguments.ElementAtOrDefault(0), out _)
+            && int.TryParse(arguments.ElementAtOrDefault(1), out _)
+            && arguments.ElementAtOrDefault(2).IsFenceDirection();
+
+        public static bool IsMovePawnArgumentsValid(this IList<string> arguments) =>
+            int.TryParse(arguments.ElementAtOrDefault(0), out _)
+            && int.TryParse(arguments.ElementAtOrDefault(1), out _);
+
+        public static bool IsCommandValid(this string command) => 
+            !string.IsNullOrWhiteSpace(command);
+    }
+}
